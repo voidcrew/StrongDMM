52dbb27dcd82618e305cc1d585184bae24a62c5c
Jackriip <97582790+Jackriip@users.noreply.github.com>
2026-09-09 11:36:19 -0700

    Prepare Planet Workshop beta 0.5.13-beta.1

--- internal/env/env.go
+++ internal/env/env.go
@@ -4,7 +4,7 @@ const Undefined = "undefined"
 
 var (
 	Title    = "StrongDMM - Voidcrew"
-	Version  = "0.5.12-beta.1"
+	Version  = "0.5.13-beta.1"
 	Revision = Undefined
 	GitHub   = "https://github.com/voidcrew/StrongDMM"
 	Support  = "https://ko-fi.com/spair"
